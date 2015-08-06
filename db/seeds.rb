3a9b5e078a927d1f5b8fbd741bbce7e59c83bc70
Andrew Patterson <andrew.patterson.3001@gmail.com>
2015-08-06 08:08:18 -0700

    add scout and scoutmaster seeds

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+User.create(type: 'Scout', name: "Charlie Spires", email:
+"c@s.com", password_hash: "1234", birthday: "01/01/2000")
+
+User.create(type: 'Scout', name: "Will Eabel", email:
+"w@e.com", password_hash: "1234", birthday: "12/12/1997")
+
+User.create(type: 'Scoutmaster', name: "Walter Rush", email:
+"w@r.com", password_hash: "1234", birthday: "01/02/1948")
+
