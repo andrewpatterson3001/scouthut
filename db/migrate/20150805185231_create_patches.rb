e2f7fcad5908fab8042d0bf7541e45e3f1f8cee5
Andrew Patterson <andrew.patterson.3001@gmail.com>
2015-08-05 19:05:46 -0700

    add property tables for objects

--- db/migrate/20150805185231_create_patches.rb
+++ db/migrate/20150805185231_create_patches.rb
@@ -1,4 +1,10 @@
 class CreatePatches < ActiveRecord::Migration
   def change
+    create_table :patches do |t|
+      t.string :evidence
+      t.integer :rank_id
+      t.integer :scout_id
+      t.timestamps
+    end
   end
 end
--- db/migrate/20150805185236_create_ranks.rb
+++ db/migrate/20150805185236_create_ranks.rb
@@ -1,4 +1,10 @@
 class CreateRanks < ActiveRecord::Migration
   def change
+    create_table :ranks do |t|
+      t.string :title
+      t.string :descriptive_url
+      t.string :image
+      t.timestamps
+    end
   end
 end
